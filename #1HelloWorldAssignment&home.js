60b6bb7918f1da7c767b70ceb4118f1dd2f3db7f
rictzyrose <ericsongabuya20@gmail.com>
2023-01-17 03:58:17 +0800

    Created #1HelloWordAsssignment&home.js

---
+++ #1HelloWorldAssignment&home.js
@@ -0,0 +1,3 @@
+console.log("Hello, world!");
+console.log("I am gonna be an awesome web developer.");
+console.log("I have the dream thats all i need i'll make it happen with some work and belief, know what i want so i'll take it on i've made mistake but mistakes make you strong. ");
